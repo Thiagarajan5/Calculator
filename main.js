54fc6cb6698518b692f20eda1716c600c4dd02c9
Thiagarajan <132124743+Thiagarajan5@users.noreply.github.com>
2025-03-16 11:20:26 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+const display = document.getElementById("display");
+
+function appenToDisplay(input){
+    display.value += input;
+}
+
+function clearDisplay() {
+    display.value = " ";
+}
+
+function calculate(){
+        display.value = eval(display.value);
+   
+    
+}
